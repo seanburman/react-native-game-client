bd01a9e6de854275ddc3f1c2b0c909bed4522add
Sean Burman <sean.burman@icloud.com>
2023-12-06 12:43:40 +0000

    Linted formatting

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,75 +1,98 @@
 import { View } from "react-native";
 import { useEffect } from "react";
-import { PanGestureHandler, TapGestureHandler } from 'react-native-gesture-handler'
+import {
+    PanGestureHandler,
+    TapGestureHandler,
+} from "react-native-gesture-handler";
 import { useCanvas } from "./context";
 
 export interface CanvasProps {
-    columns: number,
-    rows: number,
+    columns: number;
+    rows: number;
 }
 
-export const Canvas: React.FC<CanvasProps>= (props: CanvasProps) => {
+export const Canvas: React.FC<CanvasProps> = (props: CanvasProps) => {
     const {
         pixels,
         pixelDimensions,
         grid,
-        setCanvasResolution, 
+        setCanvasResolution,
         setCanvasLayout,
         setTouchCoords,
-    } = useCanvas()
+    } = useCanvas();
 
-    function handleTouchStop(){
-        setTouchCoords(undefined)
+    function handleTouchStop() {
+        setTouchCoords(undefined);
     }
 
-    function handleGestureEvent(e: {x: number, y: number}) {
-        setTouchCoords(e)
+    function handleGestureEvent(e: { x: number; y: number }) {
+        setTouchCoords(e);
     }
 
     useEffect(() => {
-        setCanvasResolution({columns: props.columns, rows: props.rows})
-    },[props.columns, props.rows])
+        setCanvasResolution({ columns: props.columns, rows: props.rows });
+    }, [props.columns, props.rows]);
 
     return (
         <TapGestureHandler
             maxDelayMs={1}
-            onGestureEvent={(e) => handleGestureEvent({x: e.nativeEvent.x, y: e.nativeEvent.y})}
-            onBegan={(e) => handleGestureEvent({x: e.nativeEvent.x as number, y: e.nativeEvent.y as number})}
-            onActivated={(e) => handleGestureEvent({x: e.nativeEvent.x as number, y: e.nativeEvent.y as number})}
-            onEnded={(handleTouchStop)}
+            onGestureEvent={(e) =>
+                handleGestureEvent({ x: e.nativeEvent.x, y: e.nativeEvent.y })
+            }
+            onBegan={(e) =>
+                handleGestureEvent({
+                    x: e.nativeEvent.x as number,
+                    y: e.nativeEvent.y as number,
+                })
+            }
+            onActivated={(e) =>
+                handleGestureEvent({
+                    x: e.nativeEvent.x as number,
+                    y: e.nativeEvent.y as number,
+                })
+            }
+            onEnded={handleTouchStop}
             onCancelled={handleTouchStop}
-            onFailed={handleTouchStop}>
+            onFailed={handleTouchStop}
+        >
             <PanGestureHandler
                 activeOffsetX={1}
                 activeOffsetY={1}
-                onGestureEvent={(e) => handleGestureEvent({x: e.nativeEvent.x, y: e.nativeEvent.y})}
+                onGestureEvent={(e) =>
+                    handleGestureEvent({
+                        x: e.nativeEvent.x,
+                        y: e.nativeEvent.y,
+                    })
+                }
                 // onBegan={handlePanStart}
                 // onActivated={handlePanStart}
                 onEnded={handleTouchStop}
                 onCancelled={handleTouchStop}
-                onFailed={handleTouchStop}>
+                onFailed={handleTouchStop}
+            >
                 <View
                     onLayout={(e) => setCanvasLayout(e.nativeEvent.layout)}
                     style={{
                         flex: 1,
-                        flexDirection: 'row',
-                        flexWrap: 'wrap'}}>
-                    {
-                        pixels?.map((p, i) => (
-                            <View
-                                style={{
-                                    width: pixelDimensions?.width,
-                                    height: pixelDimensions?.height,
-                                    backgroundColor: p.color?.HEX,
-                                    borderBottomWidth: grid ? 1 : 0,
-                                    borderRightWidth: grid ? 1 : 0,
-                                    borderColor: 'rgba(0,0,0,.25)',
-                                }}
-                                key={i}/>
-                        ))
-                    }
+                        flexDirection: "row",
+                        flexWrap: "wrap",
+                    }}
+                >
+                    {pixels?.map((p, i) => (
+                        <View
+                            style={{
+                                width: pixelDimensions?.width,
+                                height: pixelDimensions?.height,
+                                backgroundColor: p.color?.HEX,
+                                borderBottomWidth: grid ? 1 : 0,
+                                borderRightWidth: grid ? 1 : 0,
+                                borderColor: "rgba(0,0,0,.25)",
+                            }}
+                            key={i}
+                        />
+                    ))}
                 </View>
             </PanGestureHandler>
         </TapGestureHandler>
-    )
-}
+    );
+};
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,41 +1,46 @@
-import { useEffect, useState } from "react"
-import { LayoutRectangle, Modal, Pressable } from "react-native"
+import { useEffect, useState } from "react";
+import { LayoutRectangle, Modal, Pressable } from "react-native";
 
 interface Props {
-    open: boolean
-    close: () => void
+    open: boolean;
+    close: () => void;
 }
 
-export const ModalEmpty: React.FC<React.PropsWithChildren<Props>> = ({children, open, close}) => {
-    const [modalLayout, setModalLayout] = useState<LayoutRectangle>()
+export const ModalEmpty: React.FC<React.PropsWithChildren<Props>> = ({
+    children,
+    open,
+    close,
+}) => {
+    const [modalLayout, setModalLayout] = useState<LayoutRectangle>();
 
     return (
         <>
-            {
-                open &&
+            {open && (
                 <Modal
                     visible={open}
                     transparent
                     onRequestClose={close}
                     style={{
-                    flex: 1,
-                    padding: 10,
-                    borderRadius: 10,
-                    marginTop: 0}}>
-                        <Pressable
-                        style={{
                         flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(0,0,0,0.4)'}}
+                        padding: 10,
+                        borderRadius: 10,
+                        marginTop: 0,
+                    }}
+                >
+                    <Pressable
+                        style={{
+                            flex: 1,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            backgroundColor: "rgba(0,0,0,0.4)",
+                        }}
                         onPressOut={() => {}}
-                        onPress={close}>
-                            <Pressable>
-                                {children}
-                            </Pressable>
-                        </Pressable>
+                        onPress={close}
+                    >
+                        <Pressable>{children}</Pressable>
+                    </Pressable>
                 </Modal>
-            }
+            )}
         </>
-    )
-}
+    );
+};
